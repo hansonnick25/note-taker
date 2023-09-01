9e579d598fd9995ea30b0880dd61a496457d0176
Nick Hanson <hansonnick25@gmail.com>
2023-08-31 23:40:34 -0500

    Adding in get routes for notes and homepage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Declarations and Imports
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const api = require('./routes/index.js')
 const app = express()
 
@@ -10,6 +10,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', api)
 app.use(express.static('public'))
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 // Listener
 app.listen(PORT, () => {
